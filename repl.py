ade079884cbb9c81ccf3237440b83fb0a0caeadb
Halvor Linder <halvor_lh@hotmail.no>
2022-12-01 22:46:41 +0100

    make better repl

--- repl.py
+++ repl.py
@@ -1,12 +1,41 @@
-import sys, os, io
 from parser.parser import parse
 from interpreter.interpreter import interpret
 
+from cmd import Cmd
+
+ 
+class Prompt(Cmd):
+
+    prompt = '>>> '
+    # intro = "Welcome to the haellang repl!"
+
+    def init(self): 
+        self.assignment_store = dict()
+
+    def do_exit(self, _):
+        exit(0)
+    
+    def default(self, statement):
+        try:
+            self.assignment_store = execute_statement(statement, self.assignment_store)
+        except Exception as e:
+            print(e)
+ 
+ 
+    do_EOF = do_exit
+ 
+def execute_statement(statement:str, assignment_store:dict):
+    ast = parse(statement)
+    value, new_assignment_store = interpret(ast, assignment_store)
+    print(f'- {value}')
+    return new_assignment_store
+
+
 def repl():
-    assignment_store = dict()
+    prompt = Prompt()
+    prompt.init()
     while True:
-        statement = input('$ ')
-        ast = parse(statement)
-        value, assignment_store = interpret(ast, assignment_store)
-        print(f'- {value}')
-        
+        try:
+            prompt.cmdloop()
+        except KeyboardInterrupt as k:
+            print('\nKeyboardInterrupt')
